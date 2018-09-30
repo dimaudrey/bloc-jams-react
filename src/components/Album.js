e1401664fb59ba182ed575fa8366620eaf521f2a
dimaudrey <40181448+dimaudrey@users.noreply.github.com>
2018-09-30 10:45:59 -0700

    Checkpoint 6 album

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
+
 
 class Album extends Component {
+  constructor(props) {
+    super(props);
+
+    const album = albumData.find( album => {
+      return album.slug === this.props.match.params.slug
+    });
+
+      this.state = { album: album };
+  }
+
   render() {
     return (
       <section className="album">
-         {this.props.match.params.slug} Album will go here
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-colum" />
+            <col id="song-title-colum" />
+            <col id="song-duration-colum" />
+          </colgroup>
+          <tbody className = 'songList'>
+           {
+           this.state.albums.songs.map ( (song, index) =>
+            <Link to={`/album/${album.slug}`} key={index}>
+              <tr>
+                <td>{ index + 1 }</td>
+                <td>{ song.title }</td>
+                <td>{ song.duration }</td>
+              </tr>
+          </Link>
+            )
+          }
+          </tbody>
+
+        </table>
       </section>
     );
   }
